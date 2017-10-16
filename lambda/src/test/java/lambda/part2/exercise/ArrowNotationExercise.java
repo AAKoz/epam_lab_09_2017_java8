62d9470a769e125e6d52f1fcc4d2697347ff29f5
Aleksandr Kozlov <Aleksandr_Kozlov2@epam.com>
2017-10-16 03:38:12 +0300

    pushed all hometasks, i hope in time

---
+++ lambda/src/test/java/lambda/part2/exercise/ArrowNotationExercise.java
@@ -0,0 +1,71 @@
+package lambda.part2.exercise;
+
+import data.Person;
+import org.junit.Test;
+
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
+public class ArrowNotationExercise {
+
+    @Test
+    public void getAge() {
+        // Person -> Integer
+        final Function<Person, Integer> getAge = Person::getAge; // TODO
+
+        assertEquals(Integer.valueOf(33), getAge.apply(new Person("", "", 33)));
+    }
+
+    private static boolean sameAge(Person p1, Person p2) {
+        return (p1.getAge() == p2.getAge());
+    }
+
+    @Test
+    public void compareAges() {
+        // TODO use BiPredicate
+        // compareAges: (Person, Person) -> boolean
+        BiPredicate<Person,Person> compareAges = ArrowNotationExercise::sameAge;
+
+        //throw new UnsupportedOperationException("Not implemented");
+        assertEquals(true, compareAges.test(new Person("a", "b", 22), new Person("c", "d", 22)));
+    }
+
+    // TODO
+    // getFullName: Person -> String
+    private static String getFullName(Person p) {
+        return p.getFirstName() + " " + p.getLastName();
+    }
+
+    // TODO
+    // ageOfPersonWithTheLongestFullName: (Person -> String) -> ((Person, Person) -> int)
+    //
+    private static int ageOfPersonWithTheLongestFullName(Person p1, Person p2) {
+        String firstPersonsFullName = getFullName(p1);
+        String secondPersonsFullName = getFullName(p2);
+        if (firstPersonsFullName.length() >= secondPersonsFullName.length()) {
+            return p1.getAge();
+        } else {
+            return p2.getAge();
+        }
+    }
+
+    @Test
+    public void getAgeOfPersonWithTheLongestFullName() {
+        // Person -> String
+        final Function<Person, String> getFullName = ArrowNotationExercise::getFullName; // TODO
+
+        // (Person, Person) -> Integer
+        // TODO use ageOfPersonWithTheLongestFullName(getFullName)
+        final BiFunction<Person, Person, Integer> ageOfPersonWithTheLongestFullName =
+                ArrowNotationExercise::ageOfPersonWithTheLongestFullName;
+
+        assertEquals(
+                Integer.valueOf(1),
+                ageOfPersonWithTheLongestFullName.apply(
+                        new Person("a", "b", 2),
+                        new Person("aa", "b", 1)));
+    }
+}
---
+++ lambda/src/test/java/lambda/part3/exercise/Mapping.java
@@ -0,0 +1,188 @@
+package lambda.part3.exercise;
+
+import data.Employee;
+import data.JobHistoryEntry;
+import data.Person;
+import org.junit.Test;
+
+import java.util.*;
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings({"WeakerAccess"})
+public class Mapping {
+
+    private static class MapHelper<T> {
+
+        private final List<T> list;
+
+        public MapHelper(List<T> list) {
+            this.list = list;
+        }
+
+        public List<T> getList() {
+            return list;
+        }
+
+        // ([T], T -> R) -> [R]
+        public <R> MapHelper<R> map(Function<T, R> f) {
+            final List<R> res = new ArrayList<>(this.list.size());
+            for (T t: list) {
+                res.add(f.apply(t));
+            }
+            return new MapHelper<>(res);
+        }
+
+        // ([T], T -> [R]) -> [R]
+        public <R> MapHelper<R> flatMap(Function<T, List<R>> f) {
+            final List<R> res = new ArrayList<>(this.list.size());
+            for (T t: list) {
+                res.addAll(f.apply(t));
+            }
+            return new MapHelper<>(res);
+        }
+    }
+
+    public static List<JobHistoryEntry> addOneYear(List<JobHistoryEntry> a) {
+        for (int i = 0; i < a.size(); i++) {
+            a.set(i,new JobHistoryEntry(a.get(i).getDuration() + 1, a.get(i).getPosition(), a.get(i).getEmployer()));
+        }
+        return a;
+    }
+
+    public static List<JobHistoryEntry> qaToQA (List<JobHistoryEntry> a) {
+        for (JobHistoryEntry data: a) {
+            if (Objects.equals(data.getPosition(),"qa")) {
+                a.set(a.indexOf(data),data.withPosition("QA"));
+            }
+        }
+        return a;
+    }
+
+    @Test
+    public void mapping() {
+        List<Employee> employees = Arrays.asList(
+                new Employee(new Person("a", "Galt", 30),
+                        Arrays.asList(
+                                new JobHistoryEntry(2, "dev", "epam"),
+                                new JobHistoryEntry(1, "dev", "google")
+                        )),
+                new Employee(new Person("b", "Doe", 40),
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "qa", "yandex"),
+                                new JobHistoryEntry(1, "qa", "epam"),
+                                new JobHistoryEntry(1, "dev", "abc")
+                        )),
+                new Employee(new Person("c", "White", 50),
+                        Collections.singletonList(
+                                new JobHistoryEntry(5, "qa", "epam")
+                        ))
+        );
+
+        List<Employee> mappedEmployees = new MapHelper<>(employees)
+                .map(e -> e.withPerson(e.getPerson().withFirstName("John")))
+                .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
+                .map(e -> e.withJobHistory(qaToQA(e.getJobHistory())))
+                .getList();
+
+        List<Employee> expectedResult = Arrays.asList(
+                new Employee(new Person("John", "Galt", 30),
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(2, "dev", "google")
+                        )),
+                new Employee(new Person("John", "Doe", 40),
+                        Arrays.asList(
+                                new JobHistoryEntry(4, "QA", "yandex"),
+                                new JobHistoryEntry(2, "QA", "epam"),
+                                new JobHistoryEntry(2, "dev", "abc")
+                        )),
+                new Employee(new Person("John", "White", 50),
+                        Collections.singletonList(
+                                new JobHistoryEntry(6, "QA", "epam")
+                        ))
+        );
+
+        assertEquals(expectedResult, mappedEmployees);
+    }
+
+    private static class LazyMapHelper<T, R> {
+
+        private final List<T> list;
+        private final Function<T,R> function;
+
+        public LazyMapHelper(List<T> list, Function<T, R> function) {
+            this.list = list;
+            this.function = function;
+        }
+
+        public static <T> LazyMapHelper<T, T> from(List<T> list) {
+            return new LazyMapHelper<>(list, Function.identity());
+        }
+
+        public List<R> force() {
+            final List<R> res = new ArrayList<>(this.list.size());
+            for (T t: list) {
+                res.add(function.apply(t));
+            }
+            return res;
+        }
+
+        public <R2> LazyMapHelper<T, R2> map(Function<R, R2> f) {
+            return new LazyMapHelper<>(this.list, function.andThen(f));
+        }
+    }
+
+    // TODO * LazyFlatMapHelper
+
+    @Test
+    public void lazyMapping() {
+        List<Employee> employees = Arrays.asList(
+                new Employee(
+                        new Person("a", "Galt", 30),
+                        Arrays.asList(
+                                new JobHistoryEntry(2, "dev", "epam"),
+                                new JobHistoryEntry(1, "dev", "google")
+                        )),
+                new Employee(
+                        new Person("b", "Doe", 40),
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "qa", "yandex"),
+                                new JobHistoryEntry(1, "qa", "epam"),
+                                new JobHistoryEntry(1, "dev", "abc")
+                        )),
+                new Employee(
+                        new Person("c", "White", 50),
+                        Collections.singletonList(
+                                new JobHistoryEntry(5, "qa", "epam")
+                        ))
+        );
+
+        List<Employee> mappedEmployees = LazyMapHelper.from(employees)
+                .map(e -> e.withPerson(e.getPerson().withFirstName("John")))
+                .map(e -> e.withJobHistory(addOneYear(e.getJobHistory())))
+                .map(e -> e.withJobHistory(qaToQA(e.getJobHistory())))
+                .force();
+
+        List<Employee> expectedResult = Arrays.asList(
+                new Employee(new Person("John", "Galt", 30),
+                        Arrays.asList(
+                                new JobHistoryEntry(3, "dev", "epam"),
+                                new JobHistoryEntry(2, "dev", "google")
+                        )),
+                new Employee(new Person("John", "Doe", 40),
+                        Arrays.asList(
+                                new JobHistoryEntry(4, "QA", "yandex"),
+                                new JobHistoryEntry(2, "QA", "epam"),
+                                new JobHistoryEntry(2, "dev", "abc")
+                        )),
+                new Employee(new Person("John", "White", 50),
+                        Collections.singletonList(
+                                new JobHistoryEntry(6, "QA", "epam")
+                        ))
+        );
+
+        assertEquals(mappedEmployees, expectedResult);
+    }
+}
